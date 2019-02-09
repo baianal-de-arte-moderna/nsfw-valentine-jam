1fcef00edd9abc9b9b091227b4ccd8a2de6e5ed2
Vinícius Rodrigues de Uzêda <vruzeda@gmail.com>
2019-02-09 11:58:06 -0200

    Fix message command (#7)
    * Fix message command issues
    
    * Adjust click handling abstraction for the message prompt

--- Overcocked/Assets/Scripts/Commands/MessageCommand.cs
+++ Overcocked/Assets/Scripts/Commands/MessageCommand.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class MessageCommand : Command
@@ -18,6 +19,12 @@ public class MessageCommand : Command
   {
     MessagePrompt messagePrompt = canvas.GetComponentInChildren<MessagePrompt>();
     messagePrompt.CurrentMessage = message;
-    messagePrompt.OnMessageFinished += NotifyCommandExecuted;
+    messagePrompt.OnMessageFinished += OnMessageFinished;
+  }
+
+  private void OnMessageFinished(MessagePrompt messagePrompt)
+  {
+    messagePrompt.OnMessageFinished -= OnMessageFinished;
+    NotifyCommandExecuted();
   }
 }
