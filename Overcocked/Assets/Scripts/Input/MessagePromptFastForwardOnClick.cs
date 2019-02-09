6cc8734171ad37f8887c238b5260fe1d30740345
Vinicius Dias de Oliveira Gardelli <thegards@users.noreply.github.com>
2019-02-09 03:20:51 -0200

    Message prompt improvements (#4)
    * Added FastForward method to MessagePrompt, fixed MainGame scene at 1920x1080, fixed MessagePrompt design and anchors.
    
    * Added treatment for mouse input to trigger fast forward.
    
    * Fucking crappy code for hiding the name panel in the message prompt
    
    * Font adjustments to fit the text in the message prompt.
    
    * Added message finished notification system to message prompt.

---
+++ Overcocked/Assets/Scripts/Input/MessagePromptFastForwardOnClick.cs
@@ -0,0 +1,16 @@
+﻿// vim: set ts=2 sts=2 sw=2 expandtab:
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MessagePromptFastForwardOnClick : MonoBehaviour
+{
+  // Update is called once per frame
+  void Update() {
+    if (Input.GetMouseButtonDown(0)) {
+      MessagePrompt messagePrompt = GetComponent<MessagePrompt>();
+      messagePrompt.FastForward();
+    }
+  }
+}
