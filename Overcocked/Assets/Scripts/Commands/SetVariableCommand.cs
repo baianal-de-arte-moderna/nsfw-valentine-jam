c3d619a88441d8e06e4300a7bbc0d3db795c4793
Vinicius Uzeda <vruzeda@gmail.com>
2019-02-09 18:55:28 -0200

    Implement push_var_story and set_var commands

---
+++ Overcocked/Assets/Scripts/Commands/SetVariableCommand.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class SetVariableCommand : Command
+{
+  private readonly string key;
+  private readonly string value;
+
+  public SetVariableCommand(Dictionary<string, string> parameters)
+  {
+    key = parameters["key"];
+    value = parameters["value"];
+  }
+
+  public override void Execute(StoryInterpreter storyInterpreter, Canvas canvas)
+  {
+    storyInterpreter.SetVariable(key, value);
+    NotifyCommandExecuted();
+  }
+}
