20743a7314fff40174c0f277edaac7fef3830d42
Vinícius Rodrigues de Uzêda <vruzeda@gmail.com>
2019-02-09 17:36:17 -0200

    Implement background command (#12)

---
+++ Overcocked/Assets/Scripts/Commands/BackgroundCommand.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundCommand : Command
+{
+  private readonly string imageName;
+
+  public BackgroundCommand(Dictionary<string, string> parameters)
+  {
+    imageName = parameters["imageName"];
+  }
+
+  public override void Execute(StoryInterpreter storyInterpreter, Canvas canvas)
+  {
+    Background background = canvas.GetComponentInChildren<Background>();
+    background.image.sprite = Resources.Load<Sprite>($"Sprites/Backgrounds/{imageName}");
+    NotifyCommandExecuted();
+  }
+}
