f183f7af7d9383647409a83052db1f4e9f787cb9
Vinícius Rodrigues de Uzêda <vruzeda@gmail.com>
2019-02-09 16:35:14 -0200

    Implement choice command (#10)

---
+++ Overcocked/Assets/Scripts/Commands/ChoiceCommand.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class ChoiceCommand : Command
+{
+  private readonly string panelText;
+  private readonly string button0Text;
+  private readonly string button0StoryName;
+  private readonly string button1Text;
+  private readonly string button1StoryName;
+
+  private ChoicePrompt.ButtonPressedAction button0PressedAction;
+  private ChoicePrompt.ButtonPressedAction button1PressedAction;
+
+  public ChoiceCommand(Dictionary<string, string> parameters)
+  {
+    panelText = parameters["panelText"];
+    button0Text = parameters["button0Text"];
+    button0StoryName = parameters["button0StoryName"];
+    button1Text = parameters["button1Text"];
+    button1StoryName = parameters["button1StoryName"];
+  }
+
+  public override void Execute(StoryInterpreter storyInterpreter, Canvas canvas)
+  {
+    ChoicePrompt choicePrompt = canvas.GetComponentInChildren<ChoicePrompt>();
+
+    choicePrompt.panel.SetActive(true);
+    choicePrompt.panelText.text = panelText;
+
+    button0PressedAction = () =>
+    {
+      storyInterpreter.PushStory(button0StoryName);
+      DismissChoicePrompt(choicePrompt);
+    };
+    choicePrompt.button0Text.text = button0Text;
+    choicePrompt.OnButton0Pressed += button0PressedAction;
+
+    button1PressedAction = () =>
+    {
+      storyInterpreter.PushStory(button1StoryName);
+      DismissChoicePrompt(choicePrompt);
+    };
+    choicePrompt.button1Text.text = button1Text;
+    choicePrompt.OnButton1Pressed += button1PressedAction;
+  }
+
+  private void DismissChoicePrompt(ChoicePrompt choicePrompt)
+  {
+    choicePrompt.panel.SetActive(false);
+    choicePrompt.OnButton0Pressed -= button0PressedAction;
+    choicePrompt.OnButton1Pressed -= button1PressedAction;
+
+    NotifyCommandExecuted();
+  }
+}
