35bce7d79c8e04a5d583694366980d7cbe3c5b5e
Vinícius Rodrigues de Uzêda <vruzeda@gmail.com>
2019-02-09 04:09:29 -0200

    Initial command implementation (#5)
    Know Issues:
    - MessageCommand: it's passing to the next command after displaying the message without any user interaction;
    - InvalidCommand: it's not being invoked for some reason.

---
+++ Overcocked/Assets/Scripts/Commands/InvalidCommand.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class InvalidCommand : Command
+{
+  private CommandData commandData;
+
+  public InvalidCommand(CommandData commandData)
+  {
+    this.commandData = commandData;
+  }
+
+  public override void Execute(Canvas canvas)
+  {
+    Debug.LogError($"Invalid command: {commandData.command}, parameters {commandData.parameters}");
+    NotifyCommandExecuted();
+  }
+}
---
+++ Overcocked/Assets/Scripts/Commands/StoryData.cs
@@ -0,0 +1,8 @@
+﻿// vim: set ts=2 sts=2 sw=2 expandtab:
+using System.Collections.Generic;
+
+[System.Serializable]
+public class StoryData
+{
+  public List<CommandData> commandData;
+}
