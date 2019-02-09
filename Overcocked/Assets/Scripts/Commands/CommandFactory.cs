b0cccd1085bd0cb4dfdfc52dfd83ffbbecf50bcf
Vinícius Rodrigues de Uzêda <vruzeda@gmail.com>
2019-02-09 18:56:49 -0200

    Implement push_var_story and set_var commands (#13)

--- Overcocked/Assets/Scripts/Commands/CommandFactory.cs
+++ Overcocked/Assets/Scripts/Commands/CommandFactory.cs
@@ -12,6 +12,10 @@
         return new MessageCommand(commandData.parameters);
       case "push_story":
         return new PushStoryCommand(commandData.parameters);
+      case "push_var_story":
+        return new PushVarStoryCommand(commandData.parameters);
+      case "set_variable":
+        return new SetVariableCommand(commandData.parameters);
     }
 
     return new InvalidCommand(commandData);
