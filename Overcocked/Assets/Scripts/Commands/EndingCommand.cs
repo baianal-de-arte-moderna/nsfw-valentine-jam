de31edbefea83cccc4f8d5f036c51b382c6556ca
Vinicius Uzeda <vruzeda@gmail.com>
2019-02-10 20:55:09 -0200

    Implement ending command

---
+++ Overcocked/Assets/Scripts/Commands/EndingCommand.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine.SceneManagement;
+
+public class EndingCommand : Command
+{
+  public override void Execute(StoryInterpreter storyInterpreter)
+  {
+    SceneManager.LoadScene("MainMenu");
+  }
+}
