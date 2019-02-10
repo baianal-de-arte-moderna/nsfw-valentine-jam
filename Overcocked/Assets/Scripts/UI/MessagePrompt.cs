4b0c5ff77b57bb006ad9e2929ddac14e5d88e2dd
Vinicius Uzeda <vruzeda@gmail.com>
2019-02-10 18:39:56 -0200

    Implement history

--- Overcocked/Assets/Scripts/UI/MessagePrompt.cs
+++ Overcocked/Assets/Scripts/UI/MessagePrompt.cs
@@ -5,6 +5,24 @@ using UnityEngine.UI;
 
 public class MessagePrompt : MonoBehaviour
 {
+  [SerializeField]
+  private HistoryPrompt historyPrompt;
+
+  [SerializeField]
+  private GameObject messagePanelContainer;
+
+  [SerializeField]
+  private Text messageTextComponent;
+
+  [SerializeField]
+  private GameObject namePanelContainer;
+
+  [SerializeField]
+  private Text nameTextComponent;
+
+  [SerializeField]
+  public int textSpeed;
+
   public delegate void MessageFinishedAction(MessagePrompt messagePrompt);
   public event MessageFinishedAction OnMessageFinished;
 
@@ -13,13 +31,6 @@ public class MessagePrompt : MonoBehaviour
     public string Name;
     public string MessageText;
   };
-
-  public GameObject _namePanelContainer;
-  public Text _nameTextComponent;
-  public GameObject _messagePanelContainer;
-  public Text _messageTextComponent;
-
-  public int _textSpeed;
 
   private int _frameCount;
   private int _cursor;
@@ -42,12 +53,12 @@ public class MessagePrompt : MonoBehaviour
         }
         if (value.Name != "")
         {
-          _namePanelContainer.SetActive(true);
-          _nameTextComponent.text = value.Name;
+          namePanelContainer.SetActive(true);
+          nameTextComponent.text = value.Name;
         }
         else
         {
-          _namePanelContainer.SetActive(false);
+          namePanelContainer.SetActive(false);
         }
       }
       else
@@ -57,7 +68,7 @@ public class MessagePrompt : MonoBehaviour
           gameObject.SetActive(false);
         }
       }
-      _messageTextComponent.text = "";
+      messageTextComponent.text = "";
       _frameCount = 0;
       _cursor = 0;
     }
@@ -67,10 +78,15 @@ public class MessagePrompt : MonoBehaviour
 
   public void OnClick()
   {
+    if (historyPrompt.IsOpen())
+    {
+      return;
+    }
+
     if (_currentMessage != null &&
         _cursor < _currentMessage.MessageText.Length)
     {
-      _messageTextComponent.text = _currentMessage.MessageText;
+      messageTextComponent.text = _currentMessage.MessageText;
       _cursor = _currentMessage.MessageText.Length;
     }
     else
@@ -82,19 +98,19 @@ public class MessagePrompt : MonoBehaviour
 
   public void OnHistoryClick()
   {
-
+    historyPrompt.Open(history);
   }
 
   void FixedUpdate()
   {
     if (_currentMessage != null
         && _cursor < _currentMessage.MessageText.Length
-        && (_frameCount++ % _textSpeed) == 0)
+        && (_frameCount++ % textSpeed) == 0)
     {
       char nextChar = _currentMessage.MessageText[_cursor];
       do
       {
-        _messageTextComponent.text += nextChar;
+        messageTextComponent.text += nextChar;
         _cursor++;
       } while ((_cursor < _currentMessage.MessageText.Length) &&
           (nextChar = _currentMessage.MessageText[_cursor]) == ' ');
