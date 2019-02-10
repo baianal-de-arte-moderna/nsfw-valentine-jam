6ba5fad6a6662dbb21fb21574f31eb16e4030cdf
Vinicius Uzeda <vruzeda@gmail.com>
2019-02-10 02:15:09 -0200

    Remove characters from the scene as well

--- Overcocked/Assets/Scripts/SceneManagement/CharacterStage.cs
+++ Overcocked/Assets/Scripts/SceneManagement/CharacterStage.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,39 +6,47 @@ public class CharacterStage : MonoBehaviour
 {
   private List<CharacterScript> characterList;
 
-  void Start() {
-    this.characterList = new List<CharacterScript>();
+  void Start()
+  {
+    characterList = new List<CharacterScript>();
   }
 
-  public void Clear() {
-    this.characterList.Clear();
+  public void Clear()
+  {
+    foreach (CharacterScript character in characterList)
+    {
+      Destroy(character.gameObject);
+    }
+    characterList.Clear();
   }
 
-  public void AddCharacter(string characterName, string characterState) {
+  public void AddCharacter(string characterName, string characterState)
+  {
     CharacterScript character = Instantiate(Resources.Load<CharacterScript>("Characters/" + characterName));
 
-    this.characterList.Add(character);
+    characterList.Add(character);
 
-    CharacterScript.CharacterState state;
-    if (Enum.TryParse<CharacterScript.CharacterState>(characterState,
-          out state)) {
+    if (Enum.TryParse(characterState, out CharacterScript.CharacterState state))
+    {
       character.Emote(state);
     }
     RearrangeCharacters();
   }
 
-  private void RearrangeCharacters() {
+  private void RearrangeCharacters()
+  {
     float vCenter = 0.5f;
-    float hStep = 1.0f / (float)(characterList.Count + 1);
+    float hStep = 1.0f / (characterList.Count + 1);
 
     float currentPosition = hStep;
 
-    foreach(var character in this.characterList) {
+    foreach (var character in characterList)
+    {
       Transform characterTransform = character.GetComponent<Transform>();
-      characterTransform.position = Camera.main.ViewportToWorldPoint(new Vector3(currentPosition,vCenter,1));
+      characterTransform.position = Camera.main.ViewportToWorldPoint(new Vector3(currentPosition, vCenter, 1));
       currentPosition += hStep;
     }
   }
 
-  void Update() {}
+  void Update() { }
 }
