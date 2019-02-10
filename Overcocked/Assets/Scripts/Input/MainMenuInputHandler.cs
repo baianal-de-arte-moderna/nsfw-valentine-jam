4dae0b588f7f0fceeee76f2414a929f853c95d90
Vinicius Dias de Oliveira Gardelli <thegards@users.noreply.github.com>
2019-02-10 16:16:18 -0200

    Added main menu draft. (#28)

---
+++ Overcocked/Assets/Scripts/Input/MainMenuInputHandler.cs
@@ -0,0 +1,36 @@
+﻿// vim: set ts=2 sts=2 sw=2 expandtab:
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuInputHandler : MonoBehaviour
+{
+  public void OnStartButtonClicked()
+  {
+    DoStartPlay();
+  }
+
+  public void OnExitButtonClicked()
+  {
+    DoExit();
+  }
+
+  private void DoStartPlay()
+  {
+    SceneManager.LoadScene("MainGame");
+  }
+
+  private void DoExit()
+  {
+    Application.Quit();
+  }
+
+  void Start() { }
+
+  void Update() {
+    if (Input.GetAxis("Cancel") != 0.0f) {
+      DoExit();
+    }
+  }
+}
