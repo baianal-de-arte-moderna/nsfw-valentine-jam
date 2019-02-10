3d5fefa1a076cdd5d5d2645debd1145cd8b2672d
Vinicius Uzeda <vruzeda@gmail.com>
2019-02-10 12:00:18 -0200

    Implement pause command

---
+++ Overcocked/Assets/Scripts/Commands/PauseCommand.cs
@@ -0,0 +1,15 @@
+﻿public class PauseCommand : Command
+{
+  private OnClickHandler.MouckClickAction mouseClickAction;
+
+  public override void Execute(StoryInterpreter storyInterpreter)
+  {
+    OnClickHandler onClickHandler = storyInterpreter.GetOnClickHandler();
+    mouseClickAction = () =>
+    {
+      onClickHandler.OnMouseClicked -= mouseClickAction;
+      NotifyCommandExecuted();
+    };
+    onClickHandler.OnMouseClicked += mouseClickAction;
+  }
+}
